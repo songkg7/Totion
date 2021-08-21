1af05785cf3dd1fe465722ec6e2f66056ad618f0
Haril Song <songkg7@gmail.com>
2021-08-22 00:40:57 +0900

    init

---
+++ src/main/java/com/lambda/totion/TotionApplication.java
@@ -0,0 +1,13 @@
+package com.lambda.totion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TotionApplication.class, args);
+    }
+
+}
