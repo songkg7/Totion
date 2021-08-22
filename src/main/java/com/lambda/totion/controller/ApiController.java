aec8b4ce84d9e25bdd0ef795955b4ab4faca86cd
Haril Song <songkg7@gmail.com>
2021-08-23 04:28:36 +0900

    feat: 가져오는 Notion 데이터를 stream 을 사용하여 정제

--- src/main/java/com/lambda/totion/controller/ApiController.java
+++ src/main/java/com/lambda/totion/controller/ApiController.java
@@ -2,8 +2,11 @@ package com.lambda.totion.controller;
 
 import com.lambda.totion.dto.AccessToken;
 import com.lambda.totion.dto.ResponseDto;
+import com.lambda.totion.dto.Result;
 import com.lambda.totion.service.NotionService;
 import com.lambda.totion.service.TistoryService;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,24 +41,24 @@ public class ApiController {
 
     }
 
-    /**
-     * status 조건으로 노션에 작성된 게시글 정보 가져오기
-     *
-     * @param status Ready, Complete
-     * @return notion table 에 작성한 게시글 데이터
-     */
+    // Notion 에 작성된 글 ID 가져오기
     @GetMapping("/table/{status}")
-    public ResponseDto status(@PathVariable String status) {
+    public List<String> status(@PathVariable String status) {
         log.info("MainController.status");
-        return notionService.getTableFilterBy(status);
+        ResponseDto response = notionService.getTableFilterBy(status);
+
+        // response 에서 id 만 가져오기
+        List<String> collect = response.getResults().stream().map(Result::getId)
+                .collect(Collectors.toList());
+
+        return collect;
+
     }
 
-    // GET: https://api.notion.com/v1/blocks/c11e8f64-d017-4cf8-8284-2dbbca6b30d6/children
+    // notion 글 id 로 내용 가져오기
     @GetMapping("/bulletin/{bulletinId}")
     public ResponseDto getBulletin(@PathVariable String bulletinId) {
         return notionService.getBulletinById(bulletinId);
     }
-
-    // 개별 게시글 내용 확인하기 - status() 를 통해 가져온 게시글의 아이디값 필요
 
 }
